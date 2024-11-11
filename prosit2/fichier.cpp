2b110d8dc6a040d6566714fe19ff7b63536da927
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-11 17:12:23 +0100

    Update fichier.cpp

--- prosit2/fichier.cpp
+++ prosit2/fichier.cpp
@@ -3,15 +3,25 @@
 
 //#include <fichier.h>
 
-Fichhier :: lecture
-ofstream monFlux("C:/Nanoc/scores.txt"); //entre guillement le chemin du fichier, monFlux est le nom du contenue du fichier
+Fichhier :: lecture(){
+    string const nomFichier("C:/Users/thais/Documents Local/CPI A2/Projet 2/testprosit2.txt"); //entre guillement le chemin du fichier, monFlux est le nom du contenue du fichier
+    ofstream monFlux(nomFichier.c_str());
+}
 
+Fichier :: ecriture(){
+    string const nomFichier("C:/Users/thais/Documents Local/CPI A2/Projet 2/testprosit2.txt");
+    ofstream monFlux(nomFichier.c_str());
 
+    if(monFlux)    
+    {
+        monFlux << "Bonjour, je suis une phrase écrite dans un fichier." << std::endl;
+        monFlux << 42.1337 << std::endl;
+        int age(36);
+        monFlux << "J'ai " << age << " ans." << std::endl;
+    }
+    else
+    {
+        std::cout << "ERREUR: Impossible d'ouvrir le fichier." << std::endl;
+    }
 
-
-
-/*
-string const nomFichier("C:/Nanoc/scores.txt");
-ofstream monFlux(nomFichier.c_str());
-*/ 
-//Déclaration d'un flux permettant d'écrire dans un fichier.
+}
