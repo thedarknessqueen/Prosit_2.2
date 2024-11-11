81a844dfa667f6413bab0dd7792c3d5b5ae71e2e
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-11 16:52:03 +0100

    Update fichier.cpp

--- prosit2/fichier.cpp
+++ prosit2/fichier.cpp
@@ -1,1 +1,17 @@
-#include <fichier.h> 
+#include <iostream>//entrée/sortie 
+#include <fstream>//fichier
+
+//#include <fichier.h>
+
+Fichhier :: lecture
+ofstream monFlux("C:/Nanoc/scores.txt"); //entre guillement le chemin du fichier, monFlux est le nom du contenue du fichier
+
+
+
+
+
+/*
+string const nomFichier("C:/Nanoc/scores.txt");
+ofstream monFlux(nomFichier.c_str());
+*/ 
+//Déclaration d'un flux permettant d'écrire dans un fichier.
