5416bec61c6ad37eb60dab4a13085055ffbfd7cb
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-12 11:49:03 +0100

    Update fichier.h

--- prosit2/fichier.h
+++ prosit2/fichier.h
@@ -1,1 +1,19 @@
-//création de la classe
+#ifndef FICHIER_H // Prévenir les inclusions multiples
+#define FICHIER_H
+
+#include <string>
+
+class Fichier {
+public:
+    // Constructeur (optionnel, selon si vous avez besoin d'initialiser quelque chose)
+    Fichier();
+
+    // Méthodes pour la lecture et l'écriture
+    void lecture();
+    void ecriture();
+
+private:
+    std::string nomFichier; // Stocke le nom ou le chemin du fichier
+};
+
+#endif // FICHIER_H
