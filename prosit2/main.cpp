c0f773ea4ab40babaaa338da623499aaedb4bfb9
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-12 11:47:40 +0100

    Update main.cpp

--- prosit2/main.cpp
+++ prosit2/main.cpp
@@ -1,80 +1,67 @@
-/*#include <cassert>
-#include <iostream>//entrée/sortie 
+#include <iostream>//entrée/sortie
 #include <fstream>//fichier
-#include <math.h>
 
 #include <encrypt.h>
 #include <decrypt.h>
 #include <fichier.h>
+
+using namespace std;
+
 int action;
 int code;
+int decal;
+
+
 
 int main()
 {
-    std::cout << "Que voulez vous faire ? \n 1. encrypter un fichier texte. \n 2. décrypter un fichier texte." << std::endl;
-    std::cin >> action ;
+    cout << "Que voulez vous faire ? \n 1. encrypter un fichier texte. \n 2. décrypter un fichier texte." << endl;
+    cin >> action ;
     if action==1 {
-        std::cout << "Que voulez vous faire ? \n 1. encrypter en code CESAR. 2. encrypter en code XOR. 3. encrypter en code CESAR puis XOR." << std::endl;
-        std::cin >> code ;
+        cout << "Que voulez vous faire ? \n 1. encrypter en code CESAR. 2. encrypter en code XOR. 3. encrypter en code CESAR puis XOR." << endl;
+        cin >> code ;
         if code==1 {
-            encrypt::code_cesar();// appel de la fonction 
+            cout << "De combien de caractère voulez vous décaler ? \n" << endl;
+            cin >> decal ;
+            encrypt::code_cesar(Fichier::lecture(), int decal);// appel de la fonction
         }
         if code==2{
             encrypt::code_xor();//appel de la fonction
         }
         if code==3{
-            encrypt::code_cesar();//appel de la fonction
+            cout << "De combien de caractère voulez vous décaler votre CESAR ? \n" << endl;
+            cin >> decal ;
+            encrypt::code_cesar(Fichier::lecture(), int decal);//appel de la fonction
             encrypt::code_xor();//appel de la fonction
         }
         else{
-            std::cout << "Entrez un nombre valide pour le choix du mode d'encryption.">>std::endl;
+            cout << "Entrez un nombre valide pour le choix du mode d'encryption.">>endl;
         }
     }
     if action==2 {
-        std::cout << "Que voulez vous faire ? \n 1. desencrypter en code CESAR. 2. desencrypter en code XOR. 3. desencrypter en code XOR puis CESAR." << std::endl;
-        std::cin >> code ;
+        cout << "Que voulez vous faire ? \n 1. desencrypter en code CESAR. 2. desencrypter en code XOR. 3. desencrypter en code XOR puis CESAR." << endl;
+        cin >> code ;
         if code==1 {
-            decrypt::code_cesar();// appel de la fonction 
+            cout << "De combien de caractère est votre décalage ? \n" << endl;
+            cin >> decal ;
+            decrypt::code_cesar(Fichier::lecture(), int decal);// appel de la fonction
         }
         if code==2{
             decrypt::code_xor();//appel de la fonction
         }
         if code==3{
-            decrypt::code_cesar();//appel de la fonction
+            cout << "De combien de caractère est votre décalage CESAR ? \n" << endl;
+            cin >> decal ;
+            decrypt::code_cesar(Fichier::lecture(), int decal);//appel de la fonction
             decrypt::code_xor();//appel de la fonction
         }
         else{
-            std::cout << "Entrez un nombre valide pour le choix du mode de désencryption.">>std::endl;
+            cout << "Entrez un nombre valide pour le choix du mode de désencryption.">>endl;
         }
     }
     else{
-            std::cout << "Entrez un nombre valide pour le choix de l'action.">>std::endl;
+            cout << "Entrez un nombre valide pour le choix de l'action.">>endl;
     }
     
     
-}*/
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
-
- 
-using namespace std;
- 
-int main()
-{
-        ifstream fichier("test.txt", ios::in);  // on ouvre en lecture
- 
-        if(fichier)  // si l'ouverture a fonctionné
-        {
-                string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
-                getline(fichier, contenu);  // on met dans "contenu" la ligne
-                cout << contenu;  // on affiche la ligne
- 
-                fichier.close();
-        }
-        else
-                cerr << "Impossible d'ouvrir le fichier !" << endl;
- 
-        return 0;
 }
