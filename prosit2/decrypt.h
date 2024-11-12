397d150cf2fa89589a90918f7df42e1f60553110
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-12 11:48:27 +0100

    Update decrypt.h

--- prosit2/decrypt.h
+++ prosit2/decrypt.h
@@ -1,1 +1,15 @@
+#include <fichier.h>
+
+
+class decrypt{
+    public :
+    string texte;
+
+    decrypt () : texte(1){}
+
+    code_cesar(Fichier::lecture(), int decal){}
+    code_xor(Fichier::lecture(), int decal){}//////////////////////// a modifier
+
+
+};
 
