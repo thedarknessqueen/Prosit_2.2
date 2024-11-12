23edb37666c63355e5b5a482fff2d99f5b03c0cc
Rayane le Goat <rayane.ould.herault@gmail.com>
2024-11-12 10:44:58 +0100

    Update encrypt.h

--- prosit2/encrypt.h
+++ prosit2/encrypt.h
@@ -1,1 +1,18 @@
+//
+// Created by Admin on 12/11/2024.
+//
+
+#ifndef DECRYPT_H
+#define DECRYPT_H
+
+class crypte {
+  public:
+    string crypte_cesar();
+    string crypte_Xor();
+    string crypte_Mix();
+  private:
+    string message;
+
+  };
+#endif //DECRYPT_H
 
