956b4ca50e6546a0f34788589052783786d1ee2c
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-12 10:08:05 +0100

    Update fichier.cpp

--- prosit2/fichier.cpp
+++ prosit2/fichier.cpp
@@ -1,11 +1,26 @@
-#include <iostream>//entrée/sortie 
+#include <iostream>//entrée/sortie
 #include <fstream>//fichier
 
 //#include <fichier.h>
+using namespace std;
 
-Fichhier :: lecture(){
-    string const nomFichier("C:/Users/thais/Documents Local/CPI A2/Projet 2/testprosit2.txt"); //entre guillement le chemin du fichier, monFlux est le nom du contenue du fichier
-    ofstream monFlux(nomFichier.c_str());
+Fichier :: lecture(){
+
+        string const nomFichier("C:/Users/thais/Documents Local/CPI A2/Projet 2/testprosit2.txt"); //entre guillement le chemin du fichier, monFlux est le nom du contenue du fichier
+        ifstream monFlux(nomFichier.c_str());
+
+        if(monFlux)  // si l'ouverture a fonctionné
+        {
+                string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
+                getline(monFlux, contenu);  // on met dans "contenu" la ligne
+                cout << contenu;  // on affiche la ligne
+
+                monFlux.close();
+        }
+        else
+                cout << "Impossible d'ouvrir le fichier !" << endl;
+
+        return 0;
 }
 
 Fichier :: ecriture(){
@@ -14,14 +29,14 @@ Fichier :: ecriture(){
 
     if(monFlux)    
     {
-        monFlux << "Bonjour, je suis une phrase écrite dans un fichier." << std::endl;
+        monFlux << "Bonjour, je suis une phrase écrite dans un fichier." << endl;
         monFlux << 42.1337 << std::endl;
         int age(36);
         monFlux << "J'ai " << age << " ans." << std::endl;
     }
     else
     {
-        std::cout << "ERREUR: Impossible d'ouvrir le fichier." << std::endl;
+        cout << "ERREUR: Impossible d'ouvrir le fichier." << endl;
     }
 
 }
