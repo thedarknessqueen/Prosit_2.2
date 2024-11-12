b66f3150dc3b5d2c0c4ef87325ea4cc20ff636fd
Rayane le Goat <rayane.ould.herault@gmail.com>
2024-11-12 12:00:26 +0100

    Update encrypt.cpp

--- prosit2/encrypt.cpp
+++ prosit2/encrypt.cpp
@@ -1,1 +1,29 @@
 // fonction d'encryption
+#include "crypt.h"
+#include <iostream>
+using namespace std;
+
+crypte::crypte(string s){
+    message = s;
+
+}
+
+string crypte::crypte_Cesar(string texte,int decalage){
+    string resultat = "";
+
+    for(char c : texte){
+
+      if (isupper(c)){
+          texteChiffre += char(int(c + decalage - 65) % 26 + 65);
+      }
+      else if (islower(c)) {
+          texteChiffre += char(int(c + decalage - 97) % 26 + 97);
+    }
+      else {
+          texteChiffre += c;  // Conserver les autres caractères tels quels
+      }
+
+
+       }
+     return texteChiffre;
+}
