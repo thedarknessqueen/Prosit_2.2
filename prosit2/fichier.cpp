7ddcb8851e5f899d30435389f62be351b039f6d1
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-12 11:48:46 +0100

    Update fichier.cpp

--- prosit2/fichier.cpp
+++ prosit2/fichier.cpp
@@ -1,17 +1,20 @@
 #include <iostream>//entrée/sortie
 #include <fstream>//fichier
 
-//#include <fichier.h>
+#include <fichier.h>
+
 using namespace std;
 
-Fichier :: lecture(){
+string contenu;// déclaration d'une chaîne qui contiendra la ligne lue
+string code_final;
+
+Fichier::lecture(){
 
         string const nomFichier("C:/Users/thais/Documents Local/CPI A2/Projet 2/testprosit2.txt"); //entre guillement le chemin du fichier, monFlux est le nom du contenue du fichier
         ifstream monFlux(nomFichier.c_str());
 
         if(monFlux)  // si l'ouverture a fonctionné
         {
-                string contenu;  // déclaration d'une chaîne qui contiendra la ligne lue
                 getline(monFlux, contenu);  // on met dans "contenu" la ligne
                 cout << contenu;  // on affiche la ligne
 
@@ -23,16 +26,13 @@ Fichier :: lecture(){
         return 0;
 }
 
-Fichier :: ecriture(){
+Fichier::ecriture(){
     string const nomFichier("C:/Users/thais/Documents Local/CPI A2/Projet 2/testprosit2.txt");
     ofstream monFlux(nomFichier.c_str());
 
     if(monFlux)    
     {
-        monFlux << "Bonjour, je suis une phrase écrite dans un fichier." << endl;
-        monFlux << 42.1337 << std::endl;
-        int age(36);
-        monFlux << "J'ai " << age << " ans." << std::endl;
+        monFlux << code_final << endl;
     }
     else
     {
