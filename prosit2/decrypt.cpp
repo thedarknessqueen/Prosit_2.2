01e744a4e927d84bb8cc414068d0028edef5adcc
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-12 11:47:59 +0100

    Update decrypt.cpp

--- prosit2/decrypt.cpp
+++ prosit2/decrypt.cpp
@@ -1,1 +1,23 @@
+#include <iostream>//entrée/sortie
+
+#include <decrypt.h>
+
+using namespace std;
+
+decrypt::code_cesar(Fichier::lecture(), int decal){
+    string code_final;
+    int code_ascii = (int)contenu;
+    code_final_ascii = code_ascii - decal;
+    code_final = (string)code_final_ascii;
+    Fichier::ecriture(code_final);
+    return code_final;
+}
+decrypt::code_xor(Fichier::lecture(), int decal){//////////////////////// a modifier
+    string code_final;
+    int code_ascii = (int)contenu;
+    code_final_ascii = code_ascii + decal;
+    code_final = (string)code_final_ascii;
+    Fichier::ecriture(code_final);
+    return code_final;
+}
 
