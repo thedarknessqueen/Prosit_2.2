8aadff1d61140660ab30bf1ac2a180faa9b31f2a
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-13 23:44:47 +0100

    Update encrypt.cpp

--- prosit2/encrypt.cpp
+++ prosit2/encrypt.cpp
@@ -1,1 +1,27 @@
-// fonction d'encryption
+#include "crypt.h"
+#include <iostream>
+using namespace std;
+
+crypte::crypte(string s){
+    message = s;
+}
+
+string crypte::crypte_Cesar(string texte,int decal){
+    string texteChiffre = "";
+
+    for(char c : texte){
+
+        if (isupper(c)){
+            texteChiffre += char(int(c + decal - 65) % 26 + 65);
+        }
+        else if (islower(c)) {
+            texteChiffre += char(int(c + decal - 97) % 26 + 97);
+        }
+        else {
+            texteChiffre += c;  // Conserver les autres caractères tels quels
+        }
+
+
+    }
+    return texteChiffre;
+}
