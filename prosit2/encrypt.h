b6294dcb44a19664028f911f51d4b6efa05faa68
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-13 23:45:15 +0100

    Update encrypt.h

--- prosit2/encrypt.h
+++ prosit2/encrypt.h
@@ -1,1 +1,24 @@
+//
+// Created by Admin on 12/11/2024.
+//
+
+#ifndef CRYPT_H
+#define CRYPT_H
+#include<iostream>
+#include<string>
+
+
+class crypte {
+public:
+    std::string crypte_Cesar(std::string texte, int decal);
+    std::string crypte_Xor();
+    crypte (std::string m);
+
+private:
+    std::string message;
+
+
+
+};
+#endif //CRYPT_H
 
